7a2df38e57063b7e27a7dd1586551fa05b70ae4a
sickboy-droid <sickboyhacker@gmail.con>
2020-06-12 19:56:45 +0530

    bug fixes

--- app/src/main/java/com/gameofcoding/xlogcat/broadcasts/LogReceiver.java
+++ app/src/main/java/com/gameofcoding/xlogcat/broadcasts/LogReceiver.java
@@ -10,6 +10,7 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import com.gameofcoding.xlogcat.utils.AppConstants;
+import android.widget.Toast;
 
 public class LogReceiver extends BroadcastReceiver {
 	String TAG = "LogReceiver";
